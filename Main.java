6cc830a3df1921c65cb411a0a16a7e8948746f85
Kashaeva <karrapiisboring@gmail.com>
2023-01-20 20:01:04 +0300

    ыs

--- Main.java
+++ Main.java
@@ -1,24 +1,24 @@
 import java.sql.*;
 import java.time.LocalDate;
-
+import java.time.format.DateTimeFormatter;
 public class Main {
     static Connection conn;
     public static void main(String[] arts) throws SQL{
         conn =
-                DriverManager.getConnection();
-        LocalDate N = LocalDate.of(2023.01.18);
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/student_info");
+        LocalDate N = LocalDate.of(2023, 01,18);
         String SQL_QUERY =String.format (
                         """
-                DISTINGT SELECT teacher_fullname
+                DISTINCT SELECT teacher_fullname
                 FROM     grade_book;
                         """
 
-        )
+        , N.format(DateTimeFormatter.ofPattern("yyy-MM-dd")));
         PreparedStatement studentsQuery = conn.prepareStatement(SQL_QUERY);
-        ResultSet studentSet = studentsQuery.executeQuery();
+        ResultSet professorSet = studentsQuery.executeQuery();
         System.out.println("ФИО".repeat(10));
-        while (studentSet.next()){
-            System.out.println(studentSet.getString(1 +))
+        while (professorSet.next()){
+            System.out.println(professorSet.getString(1 )+ " " + professorSet.getString(2 )+ " "+ professorSet.getString(3 ));
         }
 
     }
